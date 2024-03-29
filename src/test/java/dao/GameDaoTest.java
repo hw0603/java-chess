c7ed770c952ad0d2cfd096237cbae85520f92f02
HyungUk Ryu <hw0603@naver.com>
2024-03-29 15:00:48 +0900

    refactor(gameDaoTest): GameDao 변경에 따른 테스트 코드 수정

--- src/test/java/dao/GameDaoTest.java
+++ src/test/java/dao/GameDaoTest.java
@@ -3,58 +3,50 @@ package dao;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.game.TeamColor;
+import java.sql.Connection;
+import java.sql.SQLException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameDaoTest {
-    final GameDao gameDao = GameDao.getInstance();
-    final PieceDao pieceDao = PieceDao.getInstance();
+    final GameDao gameDao = new GameDao();
+    Connection connection;
 
     @BeforeEach
-    void setUp() {
-        pieceDao.removeAllPieces();
-        gameDao.removeAllGames();
-        gameDao.addGame();
-        gameDao.addGame();
+    void setUp() throws SQLException {
+        connection = DBConnector.getInstance().getConnection();
+        connection.setAutoCommit(false);
     }
 
     @AfterEach
-    void tearDown() {
-        pieceDao.removeAllPieces();
-        gameDao.removeAllGames();
-    }
-
-    @Test
-    @DisplayName("현재 생성된 게임의 총 개수를 조회한다.")
-    void tupleCountTest() {
-        int count = gameDao.tupleCount();
-        assertThat(count).isEqualTo(2);
+    void tearDown() throws SQLException {
+        connection.rollback();
+        connection.close();
     }
 
     @Test
     @DisplayName("게임 정보를 추가하면 자동 생성된 게임 ID를 반환한다.")
     void addGameTest() {
-        int gameId = gameDao.addGame();
-
-        assertThat(gameDao.findTurn(gameId)).isEqualTo(TeamColor.WHITE);
-        assertThat(gameId).isEqualTo(3);
+        int gameId = gameDao.addGame(connection);
+        assertThat(gameId).isPositive();
     }
 
     @Test
     @DisplayName("게임 ID로 저장된 차례를 조회한다.")
     void findTurnTest() {
-        TeamColor turn = gameDao.findTurn(1);
-
+        int gameId = gameDao.addGame(connection);
+        TeamColor turn = gameDao.findTurn(connection, gameId);
         assertThat(turn).isEqualTo(TeamColor.WHITE);
     }
 
     @Test
-    @DisplayName("가장 최근에 저장된 게임의 차례를 조회한다.")
-    void latestGameTurnTest() {
-        TeamColor turn = gameDao.findLatestGameTurn();
-
-        assertThat(turn).isEqualTo(TeamColor.WHITE);
+    @DisplayName("저장된 게임에 대한 차례를 변경한다.")
+    void updateTurnTest() {
+        int gameId = gameDao.addGame(connection);
+        gameDao.updateTurn(connection, gameId, TeamColor.BLACK);
+        TeamColor turn = gameDao.findTurn(connection, gameId);
+        assertThat(turn).isEqualTo(TeamColor.BLACK);
     }
 }
