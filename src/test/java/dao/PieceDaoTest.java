f6ab2ca9cc6a228b2d700b474e8762b39465e013
HyungUk Ryu <hw0603@naver.com>
2024-03-29 15:15:03 +0900

    refactor(pieceDao): PieceDao 변경에 따른 테스트 코드 수정

--- src/test/java/dao/PieceDaoTest.java
+++ src/test/java/dao/PieceDaoTest.java
@@ -5,8 +5,9 @@ import static domain.Fixture.Positions.*;
 
 import domain.game.PieceFactory;
 import domain.game.PieceType;
-import domain.position.Position;
 import dto.PieceDto;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,59 +15,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PieceDaoTest {
-    private final GameDao gameDao = GameDao.getInstance();
-    private final PieceDao pieceDao = PieceDao.getInstance();
+    private final GameDao gameDao = new GameDao();
+    private final PieceDao pieceDao = new PieceDao();
+    private Connection connection;
 
     @BeforeEach
-    void setUp() {
-        pieceDao.removeAllPieces();
-        gameDao.removeAllGames();
-        int gameId1 = gameDao.addGame();
-        int gameId2 = gameDao.addGame();
-        pieceDao.addPiece(PieceDto.of(F6, PieceFactory.create(PieceType.WHITE_QUEEN)), gameId1);
-        pieceDao.addPiece(PieceDto.of(D4, PieceFactory.create(PieceType.BLACK_ROOK)), gameId2);
-        pieceDao.addPiece(PieceDto.of(F3, PieceFactory.create(PieceType.WHITE_BISHOP)), gameId2);
-        pieceDao.addPiece(PieceDto.of(A2, PieceFactory.create(PieceType.WHITE_PAWN)), gameId2);
+    void setUp() throws SQLException {
+        connection = DBConnector.getInstance().getConnection();
+        connection.setAutoCommit(false);
     }
 
     @AfterEach
-    void tearDown() {
-        pieceDao.removeAllPieces();
-        gameDao.removeAllGames();
+    void tearDown() throws SQLException {
+        connection.rollback();
+        connection.close();
     }
 
     @Test
-    @DisplayName("Piece 하나를 DB에 저장할 수 있다.")
-    void addPieceTest() {
-        Position position = A1;
-        PieceDto pieceDto = PieceDto.of(position, PieceFactory.create(PieceType.BLACK_PAWN));
-        int gameId = gameDao.addGame();
-        pieceDao.addPiece(pieceDto, gameId);
-
-        PieceDto result = pieceDao.findPiece(position.columnIndex(), position.rowIndex(), gameId);
-
-        assertThat(result).isEqualTo(pieceDto);
-    }
-
-    @Test
-    @DisplayName("저장된 모든 Piece 정보를 가져올 수 있다.")
-    void findAllPieceTest() {
-        List<PieceDto> result = pieceDao.findAllPieces(2);
-
-        List<PieceDto> expected = List.of(
-                PieceDto.of(D4, PieceFactory.create(PieceType.BLACK_ROOK)),
-                PieceDto.of(F3, PieceFactory.create(PieceType.WHITE_BISHOP)),
-                PieceDto.of(A2, PieceFactory.create(PieceType.WHITE_PAWN))
+    @DisplayName("여러 Piece 를 한 번에 저장하고 불러올 수 있다.")
+    void addPieceBulkTest() {
+        List<PieceDto> bulk = List.of(
+                PieceDto.of(A1, PieceFactory.create(PieceType.BLACK_PAWN)),
+                PieceDto.of(B2, PieceFactory.create(PieceType.WHITE_QUEEN))
         );
+        int gameId = gameDao.addGame(connection);
+        pieceDao.addAll(connection, bulk, gameId);
 
-        assertThat(result).containsAll(expected);
-    }
+        List<PieceDto> result = pieceDao.findAllPieces(connection, gameId);
 
-    @Test
-    @DisplayName("저장된 모든 Piece 정보를 삭제할 수 있다.")
-    void deleteAllPiecesTest() {
-        pieceDao.removeAllPieces();
-
-        assertThat(pieceDao.tupleCount()).isZero();
+        assertThat(result).containsAll(bulk);
     }
 }
